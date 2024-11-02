189eb9bbcd0d008298f5c5ea263cefc887f22402
Guka2009 <gukakandelaki11@gmail.com>
2024-11-02 12:57:46 +0400

    feature signup working

---
+++ sum=1+2.py
@@ -0,0 +1,105 @@
+# sum=("what do you call a bear with no teeth?")
+# sum1=(" A gummy bear!")
+
+# print(sum + sum1)
+
+
+
+# B=int(input("enter a number"))
+# A=int(input("enter a number"))
+# Total= A+B
+# print("Total= " + str(Total))
+
+
+# B=int(input("enter a number"))
+# A=int(input("enter a number"))
+# C=int(input("enter a number"))
+# Total= (A+B)*C
+# print("Total= " + str(Total))
+
+
+
+# Z=("How many Pizza slices did you have? ")
+# X=("And how many did you eat?")
+# print(Z+X)
+# had=int(input("enter number"))
+# eaten=int(input("enter number"))
+# Total= had-eaten
+# print("Total= " + str(Total))
+ 
+
+# kilogram=int(input("enter your weight in kilograms"))
+# Gamotvla= (kilogram)*2204
+
+# print("weight in k is" , kilogram , "weight in p is",Gamotvla )
+
+
+
+# num1=int(input("enter a number"))
+
+# if num1 > 10: 
+#     print("higher then 10")
+
+# if num1< 10:
+#     print("this is lower then 10") 
+
+
+# if num1 == 10:
+#     print("This is equal to 10")
+
+
+# num = int(input("Enter a number between 10 and 20: "))
+
+# if 10 <= num <= 20:
+#     print("Thank you")
+# else:
+#     print("Incorrect answer")
+
+
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print(f"{first_name} can vote")
+# elif age == 17:
+#     print(f"{first_name} {last_name} can learn to drive")
+# elif age == 16:
+#     print(f"{first_name} can go Trick-or-Treating")
+
+
+# temperature_fahrenheit = int(input("Enter the temperature in Fahrenheit: "))
+# temperature_celsius = (temperature_fahrenheit - 32) * 5 / 9
+# print(f"The temperature in Celsius is: {temperature_celsius:.1f}°C")
+
+
+# temperature_fahrenheit = int(input("Enter the temperature in Fahrenheit: "))
+# temperature_celsius = (temperature_fahrenheit - 32) * 5 / 9
+# print(f"The temperature in Celsius is: {temperature_celsius:.1f}°C")
+
+# if temperature_celsius <= 10:
+#     print("Wear warm clothes and carry an umbrella.")
+# elif 10 < temperature_celsius <= 20:
+#     print("Wear light clothes and consider bringing an umbrella.")
+# else:
+#     print("Wear thin clothes, no umbrella needed.")
+
+
+
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print(f"{number} is an even number.")
+# else:
+#     print(f"{number} is an odd number.")
+
+# remainder = number % 7   
+# if remainder == 2:
+#     print(f"When {number} is divided by 7, the remainder is 2.")
+# elif remainder == 3:
+#     print(f"When {number} is divided by 7, the remainder is 3.")
+# elif remainder == 4:
+#     print(f"When {number} is divided by 7, the remainder is 4.")
+# elif remainder == 5:
+#     print(f"When {number} is divided by 7, the remainder is 5.")
+# else:
+#     print(f"When {number} is divided by 7, the remainder is {remainder}.")
